8f46ea1176f9569a9f51399c4c515fdf67c1146a
cyberman46 <akekapon27@gmail.com>
2018-01-30 13:06:49 +0700

    Initial Commit

---
+++ Sapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sapp
+//
+//  Created by Akekapon Suebprom on 30/1/2561 BE.
+//  Copyright © 2561 kratostracking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
